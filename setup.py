4df0270eddc296b8a4f1cdb2f09a364a0e16e621
DinoMan <1238366+DinoMan@users.noreply.github.com>
2026-06-16 12:05:20 +0000

    Loosen librosa pin to >=0.9.1 for compatibility with Coqui TTS

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(name='MyShell-OpenVoice',
 
       python_requires='>=3.9',
       install_requires=[
-            'librosa==0.9.1',
+            'librosa>=0.9.1',
             'faster-whisper',
             'pydub==0.25.1',
             'wavmark==0.0.3',
